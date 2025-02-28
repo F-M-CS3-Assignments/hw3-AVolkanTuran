b95a2bd5a8eab5e476f7c818f36d92792007d7bf
Ahmet Volkan Turan <ahmetvolkanturan@Volkan-MacBook-Air.local>
2025-02-27 19:40:02 -0500

    Part2 Second Commit: Fixed the issues with reading the longer data

--- NasaLaunchAnalysis.cpp
+++ NasaLaunchAnalysis.cpp
@@ -11,9 +11,10 @@
 
 using namespace std;
 
+//Splits a string into a vector<string> by the delimiter
 vector<string> split(string str, string delim){
     vector<string> result;
-    while(str.find(delim)!=string::npos){
+    while(str.length()>0 && str.find(delim)!=string::npos){
         result.push_back(str.substr(0, str.find(delim)));
         str = str.substr(str.find(delim)+delim.length());
     }
@@ -21,34 +22,44 @@ vector<string> split(string str, string delim){
     return result;
 }
 
+//Takes in a string, finds the time stamp, and converts it into a TimeCode object
+//Invalid situations, or situations where a time stamp is not found returns -1
+//Returning -1 results in returning a TimeCode with the highest possible t value due to minimum being 0
+//This situation is never achieved by a 24 hour time stamp, so it is safe to use it as the error return value in this case
 TimeCode parse_line(string str){
     TimeCode tc;
-    vector<string> lineElements = split(str, ",");
-    if(lineElements.size()<4){
-        return NULL;
+    vector<string> lineElements = split(str, "\"");
+    if(lineElements.size()<2){
+        return -1;
     }
-    int colonIndex = lineElements.at(4).find(':');
+    int colonIndex = lineElements.at(1).find(':');
     if(colonIndex != string::npos){
-        string hrStr = lineElements.at(4).substr(colonIndex-2,2);
-        string minStr = lineElements.at(4).substr(colonIndex+1,2);
+        string hrStr = lineElements.at(1).substr(colonIndex-2,2);
+        string minStr = lineElements.at(1).substr(colonIndex+1,2);
         try{
             int hr = stoi(hrStr);
             int min = stoi(minStr);
             tc = TimeCode(hr, min, 0);
         } 
         catch(exception e){
-            return NULL;
+            return -1;
         }
+    }
+    else
+    {
+        return -1;
     }
     return tc;
 }
 
+//Main function open the file to read the data from and calculates the average of the time stamps
+//It also uses the helper function parse_line to read through the file and collect the TimeCode
 int main(){
     ifstream inFS;
     vector<TimeCode> times;
     int dataPoints = 0;
 
-    inFS.open("Space_Corrected_Short.csv");
+    inFS.open("Space_Corrected.csv");
     if(!inFS.is_open()){
         cout << "Could not open file." << endl;
         return 1;
@@ -59,7 +70,7 @@ int main(){
         getline(inFS, line);
         if(!inFS.fail()){
             TimeCode tc = parse_line(line);
-            if(tc!=NULL){
+            if(tc!=-1){
                 times.push_back(tc);
                 dataPoints++;
             }
